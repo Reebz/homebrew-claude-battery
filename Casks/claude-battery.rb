9f3b0a3aca15757e12e5f8ef7dc69e112395c547
Mitch <Reebz@users.noreply.github.com>
2026-04-06 18:03:55 +1000

    update claude-battery to v1.45

--- Casks/claude-battery.rb
+++ Casks/claude-battery.rb
@@ -1,6 +1,6 @@
 cask "claude-battery" do
-  version "1.44"
-  sha256 "f2732b479dc4e99232bd32811c7e3bc22bdbf762f7146f0a056b662673be1121"
+  version "1.45"
+  sha256 "5ea1709757f1a9732c946ab6354d25917612c23466cd37670178fd2e562cf9b3"
 
   url "https://github.com/Reebz/claude-battery/releases/download/v#{version}/claude-battery_v#{version}.dmg",
       verified: "github.com/Reebz/claude-battery/"
