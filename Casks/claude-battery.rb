e443f431375cb4297c2cfb40215ab1b9e72a8a9f
Mitch <Reebz@users.noreply.github.com>
2026-07-29 12:27:49 +1000

    update claude-battery to v1.70

--- Casks/claude-battery.rb
+++ Casks/claude-battery.rb
@@ -1,6 +1,6 @@
 cask "claude-battery" do
-  version "1.60"
-  sha256 "49ed007ff354c712365bc4cde4cf449361c5a98adf811335e8c320e1bb04d3ca"
+  version "1.70"
+  sha256 "4aab0b76f79472c8127fb148b31f2c7627ee3b809db8dc63411e350cccf4e90b"
 
   url "https://github.com/Reebz/claude-battery/releases/download/v#{version}/claude-battery_v#{version}.dmg",
       verified: "github.com/Reebz/claude-battery/"
